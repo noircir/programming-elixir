3dd36d8bf7a109e8adec1c389fff1121393d613e
Nellie <elenagithub@gmail.com>
2019-03-14 15:55:05 -0400

    fix range lower..(guess+1) - should have brackets

--- chapter-6/chop.exs
+++ chapter-6/chop.exs
@@ -4,19 +4,29 @@ defmodule Chop do
     go_guess(actual, midway(range), range)
   end
 
-  defp go_guess(actual, guess, _) when actual == guess do
-    IO.puts guess
+  def go_guess(actual, guess, _) when guess == actual do
+    IO.puts "THE ANSWER: #{actual}"
   end
 
-  defp go_guess(actual, guess, _..upper) when actual > guess do
-    guess(actual, (guess + 1)..upper)
+  def go_guess(actual, guess, _..upper) when guess < actual do
+    guess(actual, (guess+1)..upper)
   end
 
-  defp go_guess(actual, guess, lower.._) when actual < guess do
-    guess(actual, lower..(guess - 1))
+  def go_guess(actual, guess, lower.._) when guess > actual do
+    guess(actual, lower..(guess-1))
   end
 
   defp midway(lower..upper) do
     lower + div(upper - lower, 2)
   end
 end
+
+# iex(4)> Chop.guess(273, 1..1000)
+# Is it 500 ?
+# Is it 250 ?
+# Is it 375 ?
+# Is it 312 ?
+# Is it 281 ?
+# Is it 265 ?
+# Is it 273 ?
+# THE ANSWER: 273
